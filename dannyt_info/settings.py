d2fd03623bf6935e8b55d907cd99599a7642d207
unknown <dtrinh403@yahoo.com>
2020-06-07 18:12:10 -0500

    added dango-heroku

--- dannyt_info/settings.py
+++ dannyt_info/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 # import json
 
 # with open('/etc/dannyt_config.json') as config_file:
@@ -155,3 +156,5 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'us-east-2'
+
+django_heroku.settings(locals())
