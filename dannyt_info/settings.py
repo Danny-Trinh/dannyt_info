c5fbf7aa24bd3394c7e51e51b4f985b55275d4f8
unknown <dtrinh403@yahoo.com>
2020-06-07 19:42:56 -0500

    added left justify

--- dannyt_info/settings.py
+++ dannyt_info/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = True
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned onherok in production!
 if os.environ.get("NO_DEBUG"):
-    DEBUG = True
+    DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'dannyt-app.herokuapp.com']
 
