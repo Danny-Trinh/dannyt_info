7c2ac5b4cf7c62747901b223f882cac38fb30bb5
unknown <dtrinh403@yahoo.com>
2020-06-13 18:47:20 -0500

    using pipenv instead of requirements.txt

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
+def pokemon_view(request):
+    resp = requests.get('https://pokeapi.co/api/v2/pokemon/ditto')
+    # if resp.status_code != 200:
+    #     # This means something went wrong.
+    #     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    print(resp.status_code)
+    return render(request, 'pokemon/home.html')
+
